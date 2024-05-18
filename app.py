b94f5285429ea036d19114cadde1a9b3abbe48e9
{Akshar1199} <{aksharparekh1199@gmail.com}>
2024-05-18 18:25:13 +0530

    commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return jsonify({"message": "Hey, world!"})
+    return jsonify({"message": "Hey, Ak!"})
 
 
 @app.route('/', methods=['POST'])
