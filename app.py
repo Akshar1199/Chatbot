14ddeae72351e9137c8b9fdf8069b8f95920d3ef
{Akshar1199} <{aksharparekh1199@gmail.com}>
2024-05-20 00:35:06 +0530

    commit

--- app.py
+++ app.py
@@ -17,43 +17,55 @@ logging.basicConfig(level=logging.DEBUG)
 def get_weather():
     try:
     
-        app.logger.debug('Received request: %s', request.get_json())
+        # app.logger.debug('Received request: %s', request.get_json())
 
         request_data = request.get_json()
         city = request_data['queryResult']['parameters']['geo-city']
         date_time = request_data['queryResult']['parameters'].get('date-time', None)
+        temperature = request_data['queryResult']['parameters'].get('temperature', None)
+        print("temperature",temperature)
 
         app.logger.debug('Date-time: %s', date_time)
 
         if not date_time:
-            date_only = datetime.date.today()
+            date_obj = datetime.date.today()
+            date_only = date_obj
         else:
             try:
                 date_only = date_time.split('T')[0]
                 date_obj = datetime.datetime.strptime(date_only, "%Y-%m-%d").date()
+                print("ak",date_only,date_obj)
             except Exception as e:
                 app.logger.error("Error parsing date-time: %s", e)
                 return jsonify({"fulfillmentText": "Invalid date-time format"}), 400
 
+
         current_date = datetime.date.today()
         date_diff = (date_obj - current_date).days
+        print(date_obj, "---", date_only)
+
+        if date_diff != 10:
+            date_diff += 1
+        print("currentdate", str(current_date) + " date_diff ", date_diff)
+
 
         if date_diff > 14:
-            url = f"{BASE_FUTURE_URL}?key={API_KEY}&q={city}&dt={date_only}"
+            url = f"{BASE_FUTURE_URL}?key={API_KEY}&q={city}&dt={date_obj}"
         elif 0 <= date_diff <= 10:
             url = f"{BASE_FORECAST_URL}?key={API_KEY}&q={city}&days={date_diff}"
-        else:
+        elif date_diff == 10:
             return jsonify({"fulfillmentText": "Requested date is not valid for weather forecast."}), 400
 
+        print("url",url)
         # Log the URL for weather API request
-        app.logger.debug('Request URL: %s', url)
+        # app.logger.debug('Request URL: %s', url)
 
         # Fetch weather data from WeatherAPI
         response = requests.get(url)
         response_data = response.json()
 
         # Log the API response
-        app.logger.debug('API response: %s', response_data)
+        # app.logger.debug('API response: %s', response_data)
 
         if 'error' not in response_data:
             if date_diff > 14:
@@ -61,7 +73,11 @@ def get_weather():
             else:
                 # Find the weather data for the specific requested date
                 forecast_days = response_data['forecast']['forecastday']
-                weather_data = next((day for day in forecast_days if day['date'] == date_only), None)
+                
+                # print(type(forecast_days[0]['date']))
+                # print("date_only",type(date_only))
+                weather_data = next((day for day in forecast_days if day['date'] == str(date_only)), None)
+                
                 if weather_data:
                     weather_description = weather_data['day']['condition']['text']
                 else:
